d0a3df499670a660af3cc343b4b5116e2eedcbde
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-04-23 20:41:20 +0800

    .

---
+++ test.js
@@ -0,0 +1,10 @@
+function myFunction (myVar) {
+  if (myVar === 1) {
+    console.log('myVar        is equal to 1')
+  } else {      
+    console.log('myVar is not equal to 1')
+  }
+}
+
+myFunction(1)
+myFunction(2)
